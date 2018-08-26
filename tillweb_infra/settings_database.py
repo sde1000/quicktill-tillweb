02b6e8acd5513b8ba4c254f1d35f44a25fb5baad
Stephen Early <steve@assorted.org.uk>
2018-08-26 16:42:22 +0100

    Correct closing time on Sunday

--- tillweb_infra/settings_database.py
+++ tillweb_infra/settings_database.py
@@ -33,5 +33,5 @@ from datetime import datetime
 EVENT_TIMES = [
     (datetime(2018, 8, 31, 11, 0), datetime(2018, 9, 1, 1, 30), 2.0),
     (datetime(2018, 9, 1, 11, 0), datetime(2018, 9, 2, 1, 30), 2.0),
-    (datetime(2018, 9, 2, 11, 0), datetime(2018, 9, 2, 23, 0), 1.0),
+    (datetime(2018, 9, 2, 11, 0), datetime(2018, 9, 3, 0, 30), 1.0),
 ]
